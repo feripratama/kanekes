4cb7e4c69f71e926496c626d73960aa2747ea5ee
Igoy Nawamreh <igoynawamreh@gmail.com>
2017-12-30 20:22:05 +0700

    add sign-in and sign-up page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,14 @@ Route::get('/', function () {
     return view('landing-page');
 });
 
-Route::get('/admin', function () {
+Route::get('admin', function () {
     return view('admin');
+});
+
+Route::get('login', function () {
+    return view('auth.login');
+});
+
+Route::get('register', function () {
+    return view('auth.register');
 });
